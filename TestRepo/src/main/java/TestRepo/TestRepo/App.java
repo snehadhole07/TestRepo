9bb103d72f6d1789ca48ba769786ed88eed804d5
snehadhole07 <snehadhole07@gmail.com>
2020-06-11 17:57:02 +0530

    First commit(Created new java project)

---
+++ TestRepo/src/main/java/TestRepo/TestRepo/App.java
@@ -0,0 +1,13 @@
+package TestRepo.TestRepo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
